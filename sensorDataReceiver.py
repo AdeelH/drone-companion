11ee166bb3a01a63150ef243a6543b1d417bb1d2
Adeel <adeel_hassan93@hotmail.com>
2017-05-27 21:27:09 +0500

    do nonblocking reads, allow stopping

--- sensorDataReceiver.py
+++ sensorDataReceiver.py
@@ -1,6 +1,8 @@
 import struct
 import socket
 import threading
+import select
+import time
 
 
 class SensorDataReceiver(threading.Thread):
@@ -13,13 +15,20 @@ class SensorDataReceiver(threading.Thread):
 
 	def run(self):
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sock.setblocking(False)
 		sock.bind(('', self.port))
 		while not self.stopping:
-			try:
-				data, addr = sock.recvfrom(16)
-				self.callback(struct.unpack('iiii', data))
-			except:
-				break
+			time.sleep(0)
+			inputready, _, _ = select.select([sock], [], [], 0)
+			if len(inputready) < 1:
+				continue
+			while True:
+				try:
+					data, addr = sock.recvfrom(16)
+				except IOError:
+					# we consumed every packet from the socket and continue with the last one
+					break
+			self.callback(struct.unpack('iiii', data))
 		sock.close()
 
 	def stop(self):
